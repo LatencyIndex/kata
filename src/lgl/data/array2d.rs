3371c3c70a4d2db44425a00e70a917042765cb49
Latency Index <latency.index@gmail.com>
2025-04-22 19:20:46 +0200

    Add array2d::display_chars

--- src/lgl/data/array2d.rs
+++ src/lgl/data/array2d.rs
@@ -33,6 +33,7 @@ pub fn get_mut<T>(arr: &mut Array2<T>, index: Ix2s) -> Option<&mut T> {
     arr.get_mut(index)
 }
 
+/// In an array of shape (X,Y), Y is width (i.e. nb. columns), X is height (i.e. nb. rows)
 pub fn compact_display<T: Debug>(arr: &Array2<T>, sep: &str) -> String {
     let lines: Vec<String> = arr
         .rows()
@@ -41,6 +42,16 @@ pub fn compact_display<T: Debug>(arr: &Array2<T>, sep: &str) -> String {
             let strings: Vec<String> = row.iter().map(|x| format!("{x:?}")).collect();
             strings.join(sep)
         })
+        .collect();
+    lines.join("\n")
+}
+
+/// In an array of shape (X,Y), Y is width (i.e. nb. columns), X is height (i.e. nb. rows)
+pub fn display_chars(arr: &Array2<char>) -> String {
+    let lines: Vec<String> = arr
+        .rows()
+        .into_iter()
+        .map(|row| row.iter().collect())
         .collect();
     lines.join("\n")
 }
