d460911808b0ee81f50c9ae3d2a3043e38483b10
Latency Index <latency.index@gmail.com>
2025-04-22 19:20:46 +0200

    lgl::data::table: Add 2D indexing

--- src/lgl/data/table.rs
+++ src/lgl/data/table.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Debug, str::FromStr};
+use std::{fmt::Debug, ops::Add, str::FromStr};
 
 /// Convert text to a row-major table, with whitespace separated cells.
 pub fn read_rows_whitespace(s: &str) -> Vec<Vec<&str>> {
@@ -41,5 +41,41 @@ where
     table
         .iter()
         .map(|col| col.iter().map(|x| x.parse().unwrap()).collect())
+        .collect()
+}
+
+/// 2D index
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub struct I2(pub isize, pub isize);
+
+impl Add for I2 {
+    type Output = I2;
+    fn add(self, rhs: Self) -> Self::Output {
+        I2(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+pub fn get<T>(table: &[Vec<T>], index: I2) -> Option<&T> {
+    let i: usize = index.0.try_into().ok()?;
+    let j: usize = index.1.try_into().ok()?;
+    table.get(i).and_then(|v| v.get(j))
+}
+
+pub fn get_mut<T>(table: &mut [Vec<T>], index: I2) -> Option<&mut T> {
+    let i: usize = index.0.try_into().ok()?;
+    let j: usize = index.1.try_into().ok()?;
+    table.get_mut(i).and_then(|v| v.get_mut(j))
+}
+
+/// All the valid indexes of the given table.
+pub fn all_indexes<T>(table: &[Vec<T>]) -> Vec<I2> {
+    table
+        .iter()
+        .enumerate()
+        .flat_map(|(i, v)| {
+            v.iter()
+                .enumerate()
+                .map(move |(j, _)| I2(i as isize, j as isize))
+        })
         .collect()
 }
