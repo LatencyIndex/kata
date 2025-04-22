507d6f83bee372b8b520015ccf116097bd01ab43
Latency Index <latency.index@gmail.com>
2025-04-22 19:20:46 +0200

    AoC 2024 day 18: accelerate solution with bisection

--- src/aoc/y2024/d18.rs
+++ src/aoc/y2024/d18.rs
@@ -52,6 +52,26 @@ fn shortest_path(
     path
 }
 
+// Find largest i on [lo, hi) interval for which p(i) == false.
+// Assumes that p monotonically increases under the false < true ordering,
+// and that p(lo) == false and p(hi) == true.
+fn bisect_predicate<P>(p: P, mut lo: usize, mut hi: usize) -> usize
+where
+    P: Fn(usize) -> bool,
+{
+    // [..lo] is false
+    // [hi..] is true
+    while lo + 1 < hi {
+        let mid = lo + (hi - lo) / 2;
+        if p(mid) {
+            hi = mid;
+        } else {
+            lo = mid;
+        }
+    }
+    lo
+}
+
 const INPUT: &str = "data/y2024/d18/input";
 
 pub fn part1() -> usize {
@@ -92,17 +112,20 @@ pub fn part2() -> String {
     let shape = (71, 71);
     let start = Ix2s(0, 0);
     let goal = Ix2s(70, 70);
-    let mut maze = Array2::from_elem(shape, true);
-    // Could use bisection search if we were feeling fancy, but it's fast enough.
-    for b in falling.into_iter() {
-        *array2d::get_mut(&mut maze, b).unwrap() = false;
-        let maze_graph = to_maze_graph(&maze);
-        let costs = dijkstra(&maze_graph, start, None, |_| 1);
-        if !costs.contains_key(&goal) {
-            return format!("{},{}", b.1, b.0);
+    let maze = Array2::from_elem(shape, true);
+    let goal_is_unreachable = |i: usize| -> bool {
+        let mut maze = maze.clone();
+        for b in falling.iter().take(i) {
+            *array2d::get_mut(&mut maze, *b).unwrap() = false;
         }
-    }
-    "Not found".to_string()
+        let maze_graph = to_maze_graph(&maze);
+        let costs = dijkstra(&maze_graph, start, Some(goal), |_| 1);
+        !costs.contains_key(&goal)
+    };
+    // How many tiles can be blocked by the falling bytes, without blocking the goal.
+    let nb_reachable = bisect_predicate(goal_is_unreachable, 0, falling.len());
+    let blocking_byte = falling[nb_reachable];
+    format!("{},{}", blocking_byte.1, blocking_byte.0)
 }
 
 #[cfg(test)]
@@ -113,8 +136,13 @@ mod tests {
         assert_eq!(part1(), 284);
     }
     #[test]
-    #[ignore]
     fn test_part2() {
         assert_eq!(part2(), "51,50");
+    }
+    #[test]
+    fn test_bisect() {
+        let p = |i: usize| -> bool { 5 < i };
+        let i = bisect_predicate(p, 0, 10);
+        assert_eq!(i, 5);
     }
 }
